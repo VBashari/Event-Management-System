68eec9e90abc2adeded7093b35a5e693c6f92b79
VBashari <102989946+VBashari@users.noreply.github.com>
2023-05-20 17:37:01 +0200

    Added php api

--- api/controllers/AuthController.php
+++ api/controllers/AuthController.php
@@ -2,5 +2,10 @@
     require_once __DIR__ . '/../models/User';
 
     class AuthController {
-        
+        public function register() {
+            switch($_SERVER['REQUEST_METHOD']) {
+                case 'GET': 
+                    
+            }
+        }
     }
